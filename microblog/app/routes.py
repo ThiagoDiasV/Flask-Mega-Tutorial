aa42040cacb9df03538c74c8994dc35ab73a08b1
ThiagoDiasV <thiago76ers@gmail.com>
2019-04-04 22:25:10 -0300

    chapter 3 until receiving form data

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +18,9 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
