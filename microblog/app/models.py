40f2279f968a720d75b0c7e0721804336e4ee0e9
ThiagoDiasV <thiago76ers@gmail.com>
2019-04-16 11:48:13 -0300

    db created

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -7,4 +8,4 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User'
+        return f'<User {self.username}>'
