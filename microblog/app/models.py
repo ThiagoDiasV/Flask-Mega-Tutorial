524b2a923d9781f47b2d7208041afcba077a51fa
ThiagoDiasV <thiago76ers@gmail.com>
2019-05-29 22:40:00 -0300

    until User Registration

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from app import db, login 
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Ererê'}
     posts = [
@@ -17,14 +21,28 @@ def index():
             'body': 'The Avengers movie sucks!'
         }
     ]
-    return render_template('index.html', user=user, posts=posts)
+    return render_template('index.html', title='Home page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data},'
-              f'remember_me={form.remember_me.data}')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
