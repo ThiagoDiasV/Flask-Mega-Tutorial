31d75f24e5c45b9c6bca69542c0301b17d93dd36
ThiagoDiasV <thiago76ers@gmail.com>
2019-07-24 22:18:46 -0300

    until using jinta2 subtemplates

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 @login.user_loader
@@ -24,6 +25,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -72,3 +72,6 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+
